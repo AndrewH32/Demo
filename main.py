09eb8691da56c24f6308c92ce7a4bac723174bb3
AndrewH32 <137743705+AndrewH32@users.noreply.github.com>
2024-04-09 16:37:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio
 import pygame
 from inputs import get_gamepad
 import time
@@ -26,7 +25,7 @@ pygame.display.set_caption("Sol Hearts!")
 
 MAIN_FONT = pygame.font.SysFont("comicsans", 44)
 
-async def main():
+
 FPS = 60
 PATH = [(175, 119), (110, 70), (56, 133), (70, 481), (318, 731), (404, 680), (418, 521), (507, 475), (600, 551), (613, 715), (736, 713),
         (734, 399), (611, 357), (409, 343), (433, 257), (697, 258), (738, 123), (581, 71), (303, 78), (275, 377), (176, 388), (178, 260)]
@@ -258,7 +257,6 @@ game_info = GameInfo()
 
 while run:
     clock.tick(FPS)
-await asyncio.sleep(0) 
     draw(WIN, images, player_car, computer_car, game_info)
 
     while not game_info.started:
@@ -290,5 +288,5 @@ await asyncio.sleep(0)
         player_car.reset()
         computer_car.reset()
 
-asyncio.run(main())
+
 pygame.quit()
