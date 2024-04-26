7047bd09fb7736fc06a311f05183a29e74e2dc20
AndrewH32 <137743705+AndrewH32@users.noreply.github.com>
2024-04-26 16:46:42 -0600

    Update game_logic.js

--- game_logic.js
+++ game_logic.js
@@ -1,56 +1,166 @@
 
-            const canvas = document.getElementById("gameCanvas");
-            const ctx = canvas.getContext("2d");
+            // Game Info
+            class GameInfo {
+                constructor() {
+                    this.LEVELS = 10;
+                    this.level = 1;
+                    this.started = false;
+                    this.level_start_time = 0;
+                }
 
-            // Define image URLs
-            const SPACE = new Image();
-            SPACE.src = "imgs/space.jpg";
+                next_level() {
+                    this.level += 1;
+                    this.started = false;
+                }
 
-            const TRACK = new Image();
-            TRACK.src = "imgs/track.png";
+                reset() {
+                    this.level = 1;
+                    this.started = false;
+                    this.level_start_time = 0;
+                }
 
-            const TRACK_BORDER = new Image();
-            TRACK_BORDER.src = "imgs/track-border.png";
+                game_finished() {
+                    return this.level > this.LEVELS;
+                }
 
-            const FINISH = new Image();
-            FINISH.src = "imgs/finish.png";
+                start_level() {
+                    this.started = true;
+                    this.level_start_time = Date.now();
+                }
 
-            const GREEN_ROCKET = new Image();
-            GREEN_ROCKET.src = "imgs/green-rocket.png";
-
-            const ORANGE_ROCKET = new Image();
-            ORANGE_ROCKET.src = "imgs/orange-rocket.png";
-
-            const MAIN_FONT = "44px sans-serif";
-
-            // Define game logic variables and functions...
-
-            // Define utility functions
-            function scaleImage(img, factor) {
-                const width = Math.round(img.width * factor);
-                const height = Math.round(img.height * factor);
-                const canvas = document.createElement('canvas');
-                canvas.width = width;
-                canvas.height = height;
-                const ctx = canvas.getContext('2d');
-                ctx.drawImage(img, 0, 0, width, height);
-                return canvas;
+                get_level_time() {
+                    if (!this.started) return 0;
+                    return Math.round((Date.now() - this.level_start_time) / 1000);
+                }
             }
 
-            function blitRotateCenter(ctx, image, topLeft, angle) {
-                ctx.save();
-                ctx.translate(topLeft[0] + image.width / 2, topLeft[1] + image.height / 2);
-                ctx.rotate(angle * Math.PI / 180);
-                ctx.drawImage(image, -image.width / 2, -image.height / 2);
-                ctx.restore();
+            // Car class
+            class Car {
+                constructor(max_vel, rotation_vel, img, start_pos) {
+                    this.img = img;
+                    this.max_vel = max_vel;
+                    this.vel = 0;
+                    this.rotation_vel = rotation_vel;
+                    this.angle = 0;
+                    this.x = start_pos[0];
+                    this.y = start_pos[1];
+                    this.acceleration = 0.1;
+                }
+
+                rotate(left = false, right = false) {
+                    if (left) {
+                        this.angle += this.rotation_vel;
+                    } else if (right) {
+                        this.angle -= this.rotation_vel;
+                    }
+                }
+
+                draw(ctx) {
+                    ctx.save();
+                    ctx.translate(this.x, this.y);
+                    ctx.rotate(this.angle);
+                    ctx.drawImage(this.img, -this.img.width / 2, -this.img.height / 2);
+                    ctx.restore();
+                }
+
+                move_forward() {
+                    this.vel = Math.min(this.vel + this.acceleration, this.max_vel);
+                    this.move();
+                }
+
+                move_backward() {
+                    this.vel = Math.max(this.vel - this.acceleration, -this.max_vel / 2);
+                    this.move();
+                }
+
+                move() {
+                    const radians = this.angle * (Math.PI / 180);
+                    const vertical = Math.cos(radians) * this.vel;
+                    const horizontal = Math.sin(radians) * this.vel;
+                    this.y -= vertical;
+                    this.x -= horizontal;
+                }
+
+                collide(mask, x = 0, y = 0) {
+                    const car_mask = new ImageData(this.img.width, this.img.height);
+                    ctx.drawImage(this.img, 0, 0, this.img.width, this.img.height);
+                    const car_pixels = ctx.getImageData(0, 0, this.img.width, this.img.height);
+                    car_mask.data.set(car_pixels.data);
+                    const poi = overlapMask(car_mask, mask, this.x - x, this.y - y);
+                    return poi;
+                }
+
+                reset() {
+                    this.x = this.START_POS[0];
+                    this.y = this.START_POS[1];
+                    this.angle = 0;
+                    this.vel = 0;
+                }
             }
 
-            function blitTextCenter(ctx, font, text, x, y) {
-                ctx.font = font;
-                ctx.textAlign = 'center';
-                ctx.textBaseline = 'middle';
-                ctx.fillStyle = 'rgb(200, 200, 200)';
-                ctx.fillText(text, x, y);
+            // Player Car class
+            class PlayerCar extends Car {
+                constructor() {
+                    super(4, 4, GREEN_ROCKET, [180, 200]);
+                }
+
+                reduce_speed() {
+                    this.vel = Math.max(this.vel - this.acceleration / 2, 0);
+                    this.move();
+                }
+
+                bounce() {
+                    this.vel = -this.vel;
+                    this.move();
+                }
+            }
+
+            // Computer Car class
+            class ComputerCar extends Car {
+                constructor() {
+                    super(1, 4, ORANGE_ROCKET, [150, 200]);
+                    this.path = PATH;
+                    this.current_point = 0;
+                    this.vel = 1;
+                }
+
+                calculate_angle() {
+                    const [target_x, target_y] = this.path[this.current_point];
+                    const x_diff = target_x - this.x;
+                    const y_diff = target_y - this.y;
+                    let desired_radian_angle;
+                    if (y_diff === 0) {
+                        desired_radian_angle = Math.PI / 2;
+                    } else {
+                        desired_radian_angle = Math.atan(x_diff / y_diff);
+                    }
+                    if (target_y > this.y) {
+                        desired_radian_angle += Math.PI;
+                    }
+                    const difference_in_angle = this.angle - (desired_radian_angle * 180) / Math.PI;
+                    if (difference_in_angle >= 180) {
+                        difference_in_angle -= 360;
+                    }
+                    if (difference_in_angle > 0) {
+                        this.angle -= Math.min(this.rotation_vel, Math.abs(difference_in_angle));
+                    } else {
+                        this.angle += Math.min(this.rotation_vel, Math.abs(difference_in_angle));
+                    }
+                }
+
+                move() {
+                    this.calculate_angle();
+                    super.move_forward();
+                    if (this.vel === 0) {
+                        this.vel = 1;
+                    }
+                    if (Math.abs(this.x - this.path[this.current_point][0]) <= 10 && Math.abs(this.y - this.path[this.current_point][1]) <= 10) {
+                        this.current_point += 1;
+                    }
+                    if (this.current_point >= this.path.length) {
+                        this.current_point = 0;
+                    }
+                }
             }
 
             // Game loop function
@@ -98,5 +208,4 @@
 
             // Start the game loop
             draw();
-        
         
